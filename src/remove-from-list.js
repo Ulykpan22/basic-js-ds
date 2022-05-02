c5700c5ae23cb7f3cf1827909a2842549f2ce668
Ulykpan <ulykpan22@gmail.com>
2022-05-03 04:12:37 +0600

    commit

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let head = l;
+  let current = l;
+  let previous = null;
+  while (current) {
+    if (current.value !== k) {
+      previous = current;
+    } else {
+      if (previous === null) {
+        head = current.next;
+        previous = current;
+      } else {
+        previous.next = current.next;
+      }
+    }
+    current = current.next;
+  }
+  return head;
 }
 
 module.exports = {
